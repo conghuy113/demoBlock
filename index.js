f7c7c6175617bc5a5cfa5c2e6aca6e461c9ccb26
conghuy.fptu@gmail.com <conghuy.fptu@gmail.com>
2023-12-04 16:49:16 +0700

    file tình iu

---
+++ index.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Aaaaaa, I love u too";
+  gif.src =
+    "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxY = window.innerHeight - noBtnRect.height;
+  const maxX = window.innerWidth - noBtnRect.width;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.right = randomY + "px";
+});
